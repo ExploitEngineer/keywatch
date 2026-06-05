c6cbfb6523d003310759ac00eb97f375b082fcc5
ExploitEngineer <abdulrafayofficial.work@gmail.com>
2026-06-05 23:13:28 +0500

    refactor: update the sturcture of the project

---
+++ src/logger.cpp
@@ -0,0 +1,13 @@
+#include "logger.hpp"
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+void log(const string &text) {
+  ofstream data("data.txt", ios::app);
+
+  if (data.is_open())
+    data << text;
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "logger.hpp"
+
+int main() {
+  log("Starting the log...\n");
+  return 0;
+}
